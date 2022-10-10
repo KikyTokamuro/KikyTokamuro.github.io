6be0234a8935921c91851fe737c96aed5e31a02f
KikyTokamuro <kiky.tokamuro@yandex.ru>
2022-10-10 16:23:03 +0300

    Changed color theme to Zenburn

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll("pre code").forEach(block => {
-        hljs.highlightBlock(block);
+        hljs.highlightElement(block);
     });
 });
